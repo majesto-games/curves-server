56b3aec30f71dd3c91935be435ff1d2364760930
Pierre Krafft <kpierre@outlook.com>
2016-08-09 01:52:21 +0200

    Use postinstall instead of checking in typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/koa-bodyparser/index.d.ts" />
-/// <reference path="globals/koa-router/index.d.ts" />
-/// <reference path="globals/koa/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/ws/index.d.ts" />
